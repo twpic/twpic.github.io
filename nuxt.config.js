9a9c25b92c42c7779863e7c48b93dcfe829fa7c4
ma-sa321 <attmptmine@gmail.com>
2020-11-08 01:48:37 +0900

    update 1108

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,8 +29,10 @@ export default {
 
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
+    '@nuxtjs/axios'
   ],
-
+  axios: {
+  },
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
   }
--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,55 @@
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        twpic.github.io
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+    <div class="profile">
+      <h1>test</h1>
+    </div>
+    <div class="img">
+      <div class="img__container" v-for="item in items['posts']" :key="item.id">
+        <img class="img__item" v-bind:src="item.url" />
       </div>
     </div>
   </div>
 </template>
-
 <script>
-export default {}
+export default {
+  asyncData() {
+    const items = require('~/assets/data.json')
+    return {
+      items
+    }
+  }
+}
 </script>
-
-<style>
+<style lang="scss" scoped>
 .container {
-  margin: 0 auto;
-  min-height: 100vh;
   display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
 }
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+.profile {
+  background-color: red;
+  width: 20%;
 }
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+.img {
+  display: -webkit-box;
+  display: -webkit-flex;
+  display: -ms-flexbox;
+  display: flex;
+  flex-wrap: wrap;
+  &__container {
+    margin: 5px;
+    width: 400px;
+    height: 300px;
+    border-radius: 10px;
+    background: #f7f7f7;
+    box-shadow:  6px 6px 12px #dedede, 
+                -6px -6px 12px #ffffff;
+  }
+  &__item {
+    display: block;
+    margin: 8px;
+    width: 384px;
+    height: 284px;
+    border-radius: 10px;
+    object-fit: cover;
+  }
 }
 </style>
