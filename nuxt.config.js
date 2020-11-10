455eca67b00fea90834eb6072b46776f73a94965
ma-sa321 <attmptmine@gmail.com>
2020-11-10 18:10:40 +0900

    delete Logo.vue & fix metatitle

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'twpic.github.io',
+    title: 'yagu21.github.io',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
