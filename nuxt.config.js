4529a558e6baee801a7a8b56333e8dd92c23f0e8
ma-sa321 <attmptmine@gmail.com>
2020-12-20 18:36:29 +0900

    update&fix name 1220

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'yagu21.github.io',
+    title: 'yagucha.github.io',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
